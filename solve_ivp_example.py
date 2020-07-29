654390e42a0abfbea219f570f23b124aef62c89a
freude <misha.klymenko@gmail.com>
2020-07-29 14:35:06 +1000

    addded args to solve_ivp and made y0 being of complex type

--- solve_ivp_example.py
+++ solve_ivp_example.py
@@ -1,26 +1,25 @@
 from scipy.integrate import solve_ivp
 import numpy as np
 
-def f(t,y):
-    dy=om*y*1j-a*y
+def f(t,y, alpha, omega):
+    dy=omega*y*1j-alpha*y
     return dy
 
 a=0.1
 om=1
 ti=0
 tf=100
-y0=1
+y0=1+0*1j
 n=1000
 
 tev=np.linspace(ti,tf,n)
 
-sol=solve_ivp(f,(ti,tf),[y0],t_eval=tev)
+sol=solve_ivp(f,(ti,tf),[y0],t_eval=tev, args=(a, om))
 t=sol.t
 y=sol.y
 
 
 import matplotlib.pyplot as plt
 
-plt.plot(t,y[0])
-
+plt.plot(t, np.real(y[0]))
 plt.show()
